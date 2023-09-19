64164e193693da527aad1c14e10cd77078776944
Tiago V. de Oliveira <Tiago V. de Oliveira@DESKTOP-H0ESDVC>
2023-09-19 01:08:32 -0300

    Criação da classe de entidade de Doctor + criação de exceptions + criação de porta para banco de dados + criação de enum para status

---
+++ HospitalManagement/Core/Domain/Domain/Doctor/Exceptions/LastNameNullException.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Doctor.Exceptions
+{
+    public class LastNameNullException : Exception
+    {
+        public override string Message => "Last name cannot be null";
+    }
+}
---
+++ HospitalManagement/Core/Domain/Domain/Doctor/Ports/IDoctorRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Doctor.Ports
+{
+    public interface IDoctorRepository
+    {
+        Task<int> CreateDoctorAsync(Entities.Doctor doctor);
+        Task<Entities.Doctor> UpdateDoctorAsync(Entities.Doctor doctor);
+        Task DeleteDoctorAsync(int id);
+        Task<Entities.Doctor> GetDoctorById(int id);
+        Task<List<Entities.Doctor>> GetDoctorsAsync();
+    }
+}
