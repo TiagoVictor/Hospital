a83bc620f29e015f81f5f204d967fe5a5504a314
Tiago V. de Oliveira <Tiago V. de Oliveira@DESKTOP-H0ESDVC>
2023-09-19 03:03:38 -0300

    Criação de domain para Medical Reports + entity + exceptions + port

---
+++ HospitalManagement/Core/Domain/Domain/MedicalRecord/Ports/IMedicalRecordRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.MedicalRecord.Ports
+{
+    public interface IMedicalRecordRepository
+    {
+        Task<int> CreateMedicalRecordAsync(Entities.MedicalRecord medicalRecord);
+        Task<Entities.MedicalRecord> UpdateMedicalRecordAsync(Entities.MedicalRecord medicalRecord);
+        Task DeleteMedicalRecordAsync(int id);
+        Task<Entities.MedicalRecord> GetMedicalRecordByIdAsync(int id);
+        Task<List<Entities.MedicalRecord>> GetMedicalRecordsAsync();
+    }
+}
