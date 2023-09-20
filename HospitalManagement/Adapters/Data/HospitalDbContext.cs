8a2c8443c808fcbcb2e23b1b71cd8a39665c1d2b
Tiago V. de Oliveira <Tiago V. de Oliveira@DESKTOP-H0ESDVC>
2023-09-19 23:09:19 -0300

    Criando DbContext da aplicação

---
+++ HospitalManagement/Adapters/Data/HospitalDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Data.Doctor.Mapping;
+using Data.MedicalRecord.Mapping;
+using Data.Patient.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class HospitalDbContext : DbContext
+    {
+        public HospitalDbContext(DbContextOptions<HospitalDbContext> options) : base(options) { }
+
+        public DbSet<Domain.Doctor.Entities.Doctor> Doctors { get; set; }
+        public DbSet<Domain.Patient.Entities.Patient> Patients { get; set; }
+        public DbSet<Domain.MedicalRecord.Entities.MedicalRecord> MedicalRecords { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new DoctorMap());
+            modelBuilder.ApplyConfiguration(new PatientMap());
+            modelBuilder.ApplyConfiguration(new MedicalRecordMap());
+        }
+    }
+}
