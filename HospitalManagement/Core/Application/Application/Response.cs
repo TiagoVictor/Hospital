e3a935d351248872fe860390b629c326ccdc097b
Tiago V. de Oliveira <Tiago V. de Oliveira@DESKTOP-H0ESDVC>
2023-09-20 00:24:16 -0300

    Criação de classe de Response para a camada de Application

---
+++ HospitalManagement/Core/Application/Application/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace Application
+{
+    public enum ErrorCodes { }
+
+    public abstract class Response
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public ErrorCodes ErrorCode { get; set; }
+    }
+}
