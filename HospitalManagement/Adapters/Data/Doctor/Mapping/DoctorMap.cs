8f8b69c57b898ea6618341fcd175dd9746743200
Tiago V. de Oliveira <Tiago V. de Oliveira@DESKTOP-H0ESDVC>
2023-09-19 22:31:41 -0300

    Criação do mapeamento de Doctor

---
+++ HospitalManagement/Adapters/Data/Doctor/Mapping/DoctorMap.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Entity = Domain.Doctor.Entities;
+
+namespace Data.Doctor.Mapping
+{
+    public class DoctorMap : IEntityTypeConfiguration<Entity.Doctor>
+    {
+        public void Configure(EntityTypeBuilder<Entity.Doctor> builder)
+        {
+            builder.ToTable("Doctor");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(255);
+
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasColumnName("LastName")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(255);
+
+            builder.Property(x => x.Crm)
+                .IsRequired()
+                .HasColumnName("Crm")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(20);
+
+            builder.Property(x => x.Status)
+                .IsRequired()
+                .HasColumnName("Status")
+                .HasColumnType("INTEGER")
+                .HasMaxLength(10);
+        }
+    }
+}
