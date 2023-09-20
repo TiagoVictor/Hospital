6d065e73e9399f15144724cf02841d1ec019570c
Tiago V. de Oliveira <Tiago V. de Oliveira@DESKTOP-H0ESDVC>
2023-09-19 23:53:58 -0300

    Criando repository de MedicalRecord + ajustando interface de MedicalRecordRepository

---
+++ HospitalManagement/Adapters/Data/MedicalRecord/MedicalRecordRepository.cs
@@ -0,0 +1,64 @@
+﻿using Azure.Core;
+using Domain.MedicalRecord.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.MedicalRecord
+{
+    public class MedicalRecordRepository : IMedicalRecordRepository
+    {
+        private readonly HospitalDbContext _context;
+
+        public MedicalRecordRepository(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CreateMedicalRecordAsync(Domain.MedicalRecord.Entities.MedicalRecord medicalRecord)
+        {
+            await _context
+                .MedicalRecords
+                .AddAsync(medicalRecord);
+
+            await _context
+                .SaveChangesAsync();
+
+            return medicalRecord.Id;
+        }
+
+        public async Task DeleteMedicalRecordAsync(Domain.MedicalRecord.Entities.MedicalRecord medicalRecord)
+        {
+            _context
+                .MedicalRecords
+                .Remove(medicalRecord);
+
+            await _context
+                .SaveChangesAsync();
+        }
+
+        public async Task<Domain.MedicalRecord.Entities.MedicalRecord> GetMedicalRecordByIdAsync(int id)
+        {
+            return await _context
+                .MedicalRecords
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<Domain.MedicalRecord.Entities.MedicalRecord>> GetMedicalRecordsAsync()
+        {
+            return await _context
+                .MedicalRecords
+                .ToListAsync();
+        }
+
+        public async Task<Domain.MedicalRecord.Entities.MedicalRecord> UpdateMedicalRecordAsync(Domain.MedicalRecord.Entities.MedicalRecord medicalRecord)
+        {
+            _context
+                .MedicalRecords
+                .Update(medicalRecord);
+
+            await _context
+                .SaveChangesAsync();
+
+            return medicalRecord;
+        }
+    }
+}
