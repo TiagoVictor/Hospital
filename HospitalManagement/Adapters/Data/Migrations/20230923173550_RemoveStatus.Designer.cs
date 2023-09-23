a756f4f17bbdd21cd0c09e28a0c3c8345fb1b91a
Tiago V. de Oliveira <Tiago V. de Oliveira@DESKTOP-H0ESDVC>
2023-09-23 14:36:22 -0300

    Migration de remoção de Status

--- HospitalManagement/Adapters/Data/Migrations/20230923173550_RemoveStatus.Designer.cs
+++ HospitalManagement/Adapters/Data/Migrations/20230923173550_RemoveStatus.Designer.cs
@@ -3,6 +3,7 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(HospitalDbContext))]
-    partial class HospitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230923173550_RemoveStatus")]
+    partial class RemoveStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
